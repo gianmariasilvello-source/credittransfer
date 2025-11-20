ab7e27fb0fa058c9c4309ccd8b8c24bd034cafdf
Peter Buneman <peter@akjtegola.org>
2025-11-20 15:02:57 +0000

    message

--- theory/test1.py
+++ theory/test1.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 A = np.array([[ 0, 1, 0, 0   ],
               [ 0, 0, 1, 0   ],
               [ 0, 0, 0, 0.9 ],
--- theory/test3.py
+++ theory/test3.py
@@ -1,12 +1,14 @@
 
 # Build A
-A = np.array([[ 1, 0,   0, ],
-              [ 0,  1  ,   -2 ],
-              [ 0,  -0.5  ,   1]])
-
+A = np.array([[0,   1],
+              [0.9, 0]])
+v = np.array([1,0])
 
 I = np.eye(A.shape[0])
 
-AA = np.linalg.inv(A)
+M = I - A.T
+c= np.linalg.solve(M,v)   # (I - A^T)^{-1} v
+r = A.dot(np.ones(A.shape[0])) # total fraction redistributed by each node (row sums)
+k = np.diag(1 - r) @ c      # kudos retained
 
-print(AA)
+print(c)
